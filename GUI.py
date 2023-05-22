837319ec2a1b7880d74f8e5d4859014cad806f21
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-05-22 15:17:30 +0800

    Initialize the frame

--- GUI.py
+++ GUI.py
@@ -1,5 +1,13 @@
 from tkinter import *
 
+# Class for User Interface
 class UserInterface(Frame):
+    def __init__ (self, master):
+        # Initialize the Frame
+        Frame.__init__(self, master)
+        self.create_widgets()
+        self.calculator()
+    
+
 
 
