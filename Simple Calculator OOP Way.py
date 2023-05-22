0fe32b26722d2279f98ae3888089c54a30066a20
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-05-22 17:27:09 +0800

    Import User Interface

--- Simple Calculator OOP Way.py
+++ Simple Calculator OOP Way.py
@@ -3,28 +3,13 @@
 # 05/22/2023
 # This program will create a simple calculator in an OOP way.
 
-# Pseudocode
 # Import Tkinter
 from tkinter import *
-# Create a class for the following:
-# Class for User Interface
-    # Initialize the frame
-    # Method to create widgets
-        # Create labels and input fields for the two input values
-        # Create a label and drop-down menu for selecting the operator
-    # Method to define a function to be called when the button is pressed
-        # Retrieve the values from the input fields and operator selection
-        # Call the calculations
-        # Update the result label with the calculated value
-        # Handle the case of invalid inputs
-        # Clear the input fields after each calculation
-    # Method to trigger the calculation
-    # Method to display results
-# Class for Calculator
-    # Method to add two values
-    # Method to subtract two values
-    # Method to multiply two values
-    # Method to divide two values
-        # Display error when the case is dividing by zero
-# Call the class in the main file
+# Import UserInterface
+from GUI import UserInterface
+
+
+
+
+
 
