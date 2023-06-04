7596b59875370da3f08a88fc28449b367d4dd15f
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-06-04 13:09:05 +0800

    Move the method of removing input 2 label and entry from the widget to the new user interface

--- New_User_Interface.py
+++ New_User_Interface.py
@@ -1,4 +1,13 @@
 from tkinter import *
 from GUI import UserInterface
 
-#
+# Class for adding new method in the user interface
+class NewUserInterface(UserInterface):
+    # Method to remove input 2 label and entry from the widget when squared is pressed
+    def new_options(self, selected_operator):
+        if selected_operator == "squared":
+            self.input2_label.grid_remove()
+            self.input2.grid_remove()
+        else:
+            self.input2_label.grid(row=1, column=0)
+            self.input2.grid(row=1, column=1)
